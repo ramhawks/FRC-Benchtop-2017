df6901cbf86f3c266f67181605d1df9591594987
Ramhawks <winonaramhawks@gmail.com>
2017-01-13 17:40:32 -0600

    Fixed port

--- src/org/usfirst/frc/team3090/robot/Robot.java
+++ src/org/usfirst/frc/team3090/robot/Robot.java
@@ -41,7 +41,7 @@ public class Robot extends SampleRobot {
           /* Communicate w/navX-MXP via the MXP SPI Bus.                                     */
           /* Alternatively:  I2C.Port.kMXP, SerialPort.Port.kMXP or SerialPort.Port.kUSB     */
           /* See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details. */
-          ahrs = new AHRS(SPI.Port.kMXP); 
+          ahrs = new AHRS(I2C.Port.kMXP); 
       } catch (RuntimeException ex ) {
           DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
       }
